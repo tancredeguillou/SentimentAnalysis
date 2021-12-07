07d3dfc75554970bd1999f523baa625e71c8a806
Tancrede Guillou <tancredeguillou@gmail.com>
2021-12-07 11:56:26 +0100

    setting up the pipeline

--- sentiment.py
+++ sentiment.py
@@ -1,3 +1,21 @@
 import torch
 from torchtext.datasets import AG_NEWS
+from torchtext.vocab import build_vocab_from_iterator
+
 train_iter = AG_NEWS(split='train')
+
+# Tokenize the data
+from torchtext.data.utils import get_tokenizer
+tokenizer = get_tokenizer('basic_english')
+
+# Create vocabulary
+def yield_tokens(data_iter):
+    for _, text in data_iter:
+        yield tokenizer(text)
+
+vocab = build_vocab_from_iterator(yield_tokens(train_iter),min_freq=20, specials=["<unk>"])
+vocab.set_default_index(vocab["<unk>"])
+
+# Set up a pipeline
+text_pipeline = lambda x: vocab(tokenizer(x))
+label_pipeline = lambda x: int(x) - 1
